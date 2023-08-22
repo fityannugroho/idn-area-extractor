c7eef3fd07a0631fad942408b334e5e4b6627962
Fityan <fityannugroho@gmail.com>
2023-08-22 10:49:14 +0700

    feat: add divider words

---
+++ src/divider-words.ts
@@ -0,0 +1,35 @@
+import path from 'path';
+import { extractTxtFileRows } from './extractor.js';
+
+interface DividerWordsOptions {
+  withTypos?: boolean
+  excludedWords?: string[]
+}
+
+export default function getDividerWords(options?: DividerWordsOptions) {
+  const words: string[] = [];
+  const rows = extractTxtFileRows(path.resolve('data/divider-words.csv'), {
+    trim: true,
+    removeEmpty: true,
+  });
+
+  for (const row of rows) {
+    const [word, ...typos] = row.split(',');
+
+    if (options?.withTypos) {
+      words.push(word, ...typos);
+    } else {
+      words.push(word);
+    }
+
+    if (options?.excludedWords) {
+      for (const excludedWord of options.excludedWords) {
+        if (words.includes(excludedWord)) {
+          words.splice(words.indexOf(excludedWord), 1);
+        }
+      }
+    }
+  }
+
+  return words;
+}
---
+++ test/divider-words.spec.ts
@@ -0,0 +1,27 @@
+import { describe, expect, test } from 'vitest';
+import getDividerWords from '~/divider-words.js';
+
+describe('dividerWords', () => {
+  const typoWords = ['pemebntukan', 'pemakaran'];
+
+  test('default (without typos)', () => {
+    const words = getDividerWords();
+    expect(words).toStrictEqual(
+      expect.not.arrayContaining(typoWords),
+    );
+  });
+
+  test('with typos', () => {
+    const words = getDividerWords({ withTypos: true });
+    expect(words).toStrictEqual(
+      expect.arrayContaining(typoWords),
+    );
+  });
+
+  test('with excluded words', () => {
+    const words = getDividerWords({ excludedWords: ['pembentukan'] });
+    expect(words).toStrictEqual(
+      expect.not.arrayContaining(['pembentukan']),
+    );
+  });
+});
